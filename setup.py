e1e8df89a20bdf0f31583c7eae31270444425377
Peter Kubov <peter.kubov@skippay.cz>
2024-07-26 12:41:40 +0200

    0.7.0

--- setup.py
+++ setup.py
@@ -44,6 +44,6 @@ setup(
     name='skip-django-pynotify',
     packages=find_packages(include=['pynotify']),
     url='https://github.com/skip-pay/django-pynotify',
-    version='0.6.0',
+    version='0.7.0',
     zip_safe=False,
 )
